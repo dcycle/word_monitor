bb2b393aab38d646ec839eda8c0ccbe76d4d7823
alberto56 <albert@mediatribe.net>
2020-02-05 16:48:35 -0500

    Please squash this commit in the pull request

--- scripts/php-drupal9.sh
+++ scripts/php-drupal9.sh
@@ -5,4 +5,4 @@
 set -e
 
 echo "=> Identify deprecated code so we're ready for Drupal 9"
-docker run --rm -v "$(pwd)":/var/www/html/modules/word_monitor dcycle/drupal-check:1 word_monitor/src
+docker run --rm -v "$(pwd)":/var/www/html/modules/word_monitor dcycle/drupal-check:1.2019-12-30-21-59-43-UTC word_monitor/src
