6f5ec5b4b426f6f3deb0be1fa8a36a93618a1594
Albert Albala <albert@mediatribe.net>
2020-02-06 09:16:06 -0500

    Remove extraneous code in a test script and workaround for https://github.com/mglaman/drupal-check/issues/131 (#3)

--- scripts/test-running-environment.sh
+++ scripts/test-running-environment.sh
@@ -4,8 +4,5 @@
 #
 set -e
 
-echo '=> Running tests on a running environment.'
-URL="$(docker-compose port drupal 80)"
-
 echo 'Run some self-tests'
 docker-compose exec drupal /bin/bash -c 'drush ev "word_monitor()->selfTest();"'
