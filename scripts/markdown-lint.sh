54faa3761d524c1df2c65438b0a6daded806f2e6
alberto56 <albert@mediatribe.net>
2020-02-06 09:22:45 -0500

    Please squash this commit in the pull request

--- scripts/markdown-lint.sh
+++ scripts/markdown-lint.sh
@@ -6,4 +6,5 @@ set -e
 
 echo '=> Linting markdown.'
 echo ' (see https://github.com/dcycle/docker-markdown-lint/blob/master/README.md) '
-docker run --rm --entrypoint markdownlint -v $(pwd):/app/code dcycle/markdown-lint /app/code/README.md
+docker run --rm --entrypoint markdownlint \
+  -v "$(pwd)":/app/code dcycle/markdown-lint /app/code/README.md
