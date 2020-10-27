308d9dcbf8e2f79d0039791697927302724da6c7
alberto56 <albert@mediatribe.net>
2020-10-27 09:37:14 -0400

    Update jenkins script for https://github.com/dcycle/docker-digitalocean-php#public-vs-private-ip-addresses

--- scripts/jenkins/test.sh
+++ scripts/jenkins/test.sh
@@ -15,8 +15,13 @@ if [ -z "$DOCKERHOSTUSER" ] || [ -z "$DOCKERHOST" ]; then
 fi
 
 # Create a droplet
-IP=$(ssh "$DOCKERHOSTUSER@$DOCKERHOST" \
-  "./digitalocean/scripts/new-droplet.sh word-monitor")
+DROPLET_NAME=word-monitor
+PRIVATE_IP=$(ssh "$DOCKERHOSTUSER"@"$DOCKERHOST" \
+  "./digitalocean/scripts/new-droplet.sh $DROPLET_NAME")
+# https://github.com/dcycle/docker-digitalocean-php#public-vs-private-ip-addresses
+IP=$(ssh "$DOCKERHOSTUSER"@"$DOCKERHOST" "./digitalocean/scripts/list-droplets.sh" |grep "$PRIVATE_IP" --after-context=10|tail -1|cut -b 44-)
+echo "Created Droplet at $IP"
+sleep 90
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
   root@"$IP" \
   "git clone http://github.com/dcycle/word_monitor && \
