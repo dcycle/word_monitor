3fd377c7c7cc7394464c6c1db30b23f3e01a0d77
alberto56 <albert@mediatribe.net>
2020-11-06 08:48:50 -0500

    Fix Jenkins shell script to adhere to coding standards

--- scripts/jenkins/test.sh
+++ scripts/jenkins/test.sh
@@ -16,10 +16,11 @@ fi
 
 # Create a droplet
 DROPLET_NAME=word-monitor
-PRIVATE_IP=$(ssh "$DOCKERHOSTUSER"@"$DOCKERHOST" \
+# shellcheck disable=SC2029
+PRIVATE_IP=$(ssh "$DOCKERHOSTUSER@$DOCKERHOST" \
   "./digitalocean/scripts/new-droplet.sh $DROPLET_NAME")
 # https://github.com/dcycle/docker-digitalocean-php#public-vs-private-ip-addresses
-IP=$(ssh "$DOCKERHOSTUSER"@"$DOCKERHOST" "./digitalocean/scripts/list-droplets.sh" |grep "$PRIVATE_IP" --after-context=10|tail -1|cut -b 44-)
+IP=$(ssh "$DOCKERHOSTUSER@$DOCKERHOST" "./digitalocean/scripts/list-droplets.sh" |grep "$PRIVATE_IP" --after-context=10|tail -1|cut -b 44-)
 echo "Created Droplet at $IP"
 sleep 90
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
